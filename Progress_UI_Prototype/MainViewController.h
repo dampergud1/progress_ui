1eec1c5aa78d1c37bbf642883f747e37083e914b
Kasper Sølvstrøm <soelvstroem@Kaspers-MacBook-Pro.local>
2013-09-13 17:06:00 +0200

    added views for app flow show.

---
+++ Progress_UI_Prototype/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Progress_UI_Prototype
+//
+//  Created by Kasper Sølvstrøm on 13/09/13.
+//  Copyright (c) 2013 Kasper Sølvstrøm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Progress_UI_Prototype/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  Progress_UI_Prototype
+//
+//  Created by Kasper Sølvstrøm on 13/09/13.
+//  Copyright (c) 2013 Kasper Sølvstrøm. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
